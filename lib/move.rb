d1d919901d764bd0c2ea9a7a99985d1f000ec3e9
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-26 16:08:05 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,6 +5,6 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-def input_to_index(string)
-  index=string.to_i
+def input_to_index(input)
+  index=input.to_i
 end
